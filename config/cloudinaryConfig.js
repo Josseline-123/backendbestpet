d36fb8fc22fbbf9cfc17a9b5f4b10fa2ffc820a8
Josseline <josselinefg32@gmail.com>
2025-06-24 13:34:47 -0400

    commit 19

--- config/cloudinaryConfig.js
+++ config/cloudinaryConfig.js
@@ -1,12 +1,10 @@
 const cloudinary = require('cloudinary').v2;
 require('dotenv').config();
 
-cloudinary.config({
-  cloud_name: process.env.CLOUD_NAME,
-  api_key: process.env.CLOUD_API_KEY,
-  api_secret: process.env.CLOUD_API_SECRET,
-});
+// Cloudinary tomará automáticamente CLOUDINARY_URL desde el entorno
+cloudinary.config(); 
 
 module.exports = cloudinary;
+
 
 
